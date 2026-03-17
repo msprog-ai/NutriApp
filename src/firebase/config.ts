4c1499e32cc4a5eee3746ab00c227987ed5ae9c7
Rachellee Perez <rachelleperez19.rp@gmail.com>
2026-03-17 01:36:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4490396952-7b0c8",
+  "appId": "1:1054032841658:web:f766e4ffbd3fb8c0372b83",
+  "apiKey": "AIzaSyD7-NIffKNV6ZRpBzYOiV-eoL0ygey7Biw",
+  "authDomain": "studio-4490396952-7b0c8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1054032841658"
+};
